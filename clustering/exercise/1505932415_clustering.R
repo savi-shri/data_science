dce9cd3e1a6fca76dc9f59cfb015303d8bc24b1e
savi-shri <savita.shrivastava@gmail.com>
2017-12-17 08:37:35 -0600

    Update 1505932415_clustering.R

--- clustering/exercise/1505932415_clustering.R
+++ clustering/exercise/1505932415_clustering.R
@@ -1,6 +1,6 @@
 # This mini-project is based on the K-Means exercise from 'R in Action'
 # Go here for the original blog post and solutions
-# 
+# http://www.r-bloggers.com/k-means-clustering-from-r-in-action/
 
 
 # Exercise 0: Install these packages if you don't have them already
