1c21f57c452a4867d81daddcb88fc3274fcd5e1a
savi-shri <savita.shrivastava@gmail.com>
2017-12-27 17:05:20 -0600

    Data wrangling on captsone project data

---
+++ data_wrangling_on_spambase_data/README.md
@@ -0,0 +1,15 @@
+README
+
+Springboard Introduction to Data Science Capstone Project Information
+
+Project Titile : Machine Learning Classifier for Detecting Email Spams
+
+Project Description : Logistic Regression algorithm used for creating a machine learning classifier for detecting email spams
+
+FILES DESCRIPTION:
+
+1. data_wrangling_on_spambase_data.R - R Code of data wrangline on capstone project data 
+
+2. Spam names.csv - Download to local computer. This files is an input to data_wrangling_on_spambase_data.R script.
+
+3. Data Wrangling on Spambase Data.docx - Document describing the data and data wranling steps
---
+++ data_wrangling_on_spambase_data/data_wrangling_on_spambase_data.R
@@ -0,0 +1,41 @@
+
+# Data Wrangling on Spambase data set
+
+# Read the spambase data from https://archive.ics.uci.edu/ml/machine-learning-databases/spambase/spambase.data 
+file = "https://archive.ics.uci.edu/ml/machine-learning-databases/spambase/spambase.data"
+
+raw.data <- read.csv(file, header=F)
+
+# Read attributes (Spam names) from the file "Spam names.csv" located at local. 
+# Download the "Spam names.csv" file from "https://github.com/savi-shri/data_science/blob/master/CapstoneProject/Spam names.csv"
+
+name.var <- read.csv(file.choose())
+
+# change the name of the below Spam names attributes which have special characters in their name as below:  
+names(name.var)[49]<- "char_freq_semic"    # char_freq_;
+names(name.var)[50]<- "char_freq_openp"    # char_freq_(
+names(name.var)[51]<- "char_freq_openb"    # char_freq_[
+names(name.var)[52]<- "char_freq_excl"     # char_freq_!
+names(name.var)[53]<- "char_freq_dollar"   # char_freq_$
+names(name.var)[54]<- "char_freq_pound"    # char_freq_#
+
+# give column names in the raw.data using spam names data.
+colnames(raw.data) <- colnames(name.var)
+
+# copy the raw.data to another varianble raw
+raw <- raw.data
+
+#attached 'raw' to the R search path.  This means that the database is searched by R when evaluating a variable, 
+#so objects in the database can be accessed by simply giving their names.
+attach(raw)
+
+# Look at the data - how many rows and columns this data has, what is the data type etc.
+dim(raw)
+head(raw)
+tail(raw)
+str(raw)
+
+
+# Find out if there are missing values
+na.count.all <- data.frame(sapply(raw, function(y) sum(length(which(is.na(y))))))
+sum(is.na(raw))
